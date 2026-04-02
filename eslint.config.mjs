a7bbf23ac2f09b0cea24a35c4b73caf4a857bb9a
SuspiciousApplePie <andreiusita6@gmail.com>
2026-04-02 14:19:25 +0800

    Add eslint and prettier

---
+++ eslint.config.mjs
@@ -0,0 +1,19 @@
+import js from "@eslint/js";
+import globals from "globals";
+import { defineConfig, globalIgnores } from "eslint/config";
+import eslintConfigPrettier from "eslint-config-prettier/flat";
+
+export default defineConfig([
+  globalIgnores(["**/*.config.{js,mjs,cjs}"]),
+  js.configs.recommended,
+  { 
+    files: ["**/*.{js,mjs,cjs}"], 
+    languageOptions: { globals: globals.browser },
+    rules: {
+      eqeqeq: ["error", "smart"],
+      "no-unused-vars": ["error", {argsIgnorePattern: "^_"}],
+      "prefer-const": ["error", { ignoreReadBeforeAssign: true }]
+    },
+  },
+  eslintConfigPrettier,
+]);
