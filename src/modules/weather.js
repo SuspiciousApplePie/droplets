cc3bbff1727e99799ce97b61bf2af7665c0d9507
SuspiciousApplePie <andreiusita6@gmail.com>
2026-04-05 23:00:35 +0800

    Remove logs

--- src/modules/weather.js
+++ src/modules/weather.js
@@ -4,7 +4,6 @@ export async function fetchWeatherData(place) {
   const url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${place}?unitGroup=us&key=${VISUAL_CROSSING_API}&contentType=json`;
   const response = await fetch(url);
   if (!response.ok) throw new Error("Failed to fetch api");
-  console.log(response);
   const weatherData = await response.json();
   const { resolvedAddress, days } = weatherData;
   const formattedWeather = { resolvedAddress, days };
