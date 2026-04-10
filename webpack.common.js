868c31d6726cc9f838f72b85b8e155250c1c809c
SuspiciousApplePie <andreiusita6@gmail.com>
2026-04-10 15:43:20 +0800

    Fix config

--- webpack.common.js
+++ webpack.common.js
@@ -3,7 +3,7 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   entry: {
-    app: "./src/modules/index.js",
+    app: "./src/index.js",
   },
   plugins: [
     new HtmlWebpackPlugin({
